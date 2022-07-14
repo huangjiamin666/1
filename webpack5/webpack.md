491857da199ff5a036a0625aa3e3f9bd3c128a46
h <178080976@qq.com>
2022-07-14 11:30:19 +0800

    webpack學習

--- webpack5/webpack.md
+++ webpack5/webpack.md
@@ -1,2 +1,35 @@
-export 或者export default導出，用import導入
-moudle.exports 導出，用require導入
+## export 或者export default導出，用import導入
+## moudle.exports 導出，用require導入
+## 五個模塊：entry,output,module,plugin,loader
+## loader特定的模塊類型進行轉換
+## plugin用於更加廣汎的任務，如打包優化，資源管理，環境變量注入等
+1. postcss處理css兼容 （結合borwserslitrc)
+2. assets代替file-loader url-loader
+3. cleanWebpackPlugin 打包之前清除舊的包
+4. 複製html模板
+  new HtmlWebpackPlugin({
+    title:'',
+    template:'./public/index.html'
+  })
+5. DefinePlugin 定義全局常量
+  new DefinePlugin({
+    BASE_URL: '"./"'
+  })
+
+6. copy靜態文件到打包后的文件中
+  new CopyWebpackPlugin({
+    patterns:[
+      {
+        from:'public',
+        <!-- to: //默認不用寫 -->
+        globOptions:{
+          ignore:[
+            '**/index.html',
+            "**/.DS_Store"
+          ]
+        }
+      }
+    ]
+  })
+
+7. sourceMap是從已轉換的代碼，映射到原始的源文件
--- webpack5/媒体资源/assets处理图标字体/lg.webpack.js
+++ webpack5/媒体资源/assets处理图标字体/lg.webpack.js
@@ -1,90 +1,102 @@
 const path = require('path')
-module.exports={
-    entry: './src/index.js', // 入口文件 可以使用相对路径
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname,'dist'), // 打包后的输出路径
-        // assetModuleFilename: 'img/[name].[hash:4][ext]' // asset模块打包图片路径
-    },
-    module: {
-        rules: [
-            {
-              test: /\.css$/,
-              use: [
-                'style-loader',
-                {
-                  loader: 'css-loader',
-                  options: {
-                    importLoaders: 1, // post-loader执行完了，但是这里要重新用一下postcss-loader
-                    esModule: false,
-                  }
-                },
-                // {
-                //   loader: 'postcss-loader',
-                //   options: {
-                //     postcssOptions: {
-                //       plugins: [
-                //         require('autoprefixer'),
-                //         require('postcss-preset-env')
-                //       ]
-                //     }
-                //   }
-                // }
-                'postcss-loader'
-                
-              ]
-            },
-            {
-              test: /\.less$/,
-              use: [
-                'style-loader',
-                'css-loader',
-                // {
-                //   loader: 'postcss-loader',
-                //   options: {
-                //     postcssOptions: {
-                //       plugins: [
-                //         require('autoprefixer'),
-                //         require('postcss-preset-env')
-                //       ]
-                //     }
-                //   }
-                // },
-                'postcss-loader',
-                'less-loader'
-              ]
-            },
-            // {
-            //   test: /\.(png|svg|gif|jpe?g)$/,
-            //   type: 'asset/resource',
-            //   generator: {
-            //     filename: 'img/[name].[hash:4][ext]'
-            //   }
-            // },
-            // {
-            //   test: /\.(png|svg|gif|jpe?g)$/,
-            //   type: 'asset/inline'
-            // }
-            { // 这一块的代码 30kb一下打包成base64,30kb以上打包copy成图片并在特定文件内
-              test: /\.(png|svg|gif|jpe?g)$/,
-              type: 'asset',
-              generator: {
-                filename: 'img/[name].[hash:4][ext]'
-              },
-              parser: {
-                dataUrlCondition: {
-                  maxSize: 30 * 1024 
-                }
-              }
-            },
-            {
-              test: /\.(ttf|woff2?)$/,
-              type: 'asset/resource',
-              generator: {
-                filename: 'font/[name].[hash:3][ext]'
-              }
-            } 
-          ]
-    }
-    
+module.exports = {
+  entry: './src/index.js', // 入口文件 可以使用相对路径
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'), // 打包后的输出路径
+    // assetModuleFilename: 'img/[name].[hash:4][ext]' // asset模块打包图片路径
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1, // post-loader执行完了，但是这里要重新用一下postcss-loader
+              esModule: false,
+            }
+          },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     postcssOptions: {
+          //       plugins: [
+          //         require('autoprefixer'),
+          //         require('postcss-preset-env')
+          //       ]
+          //     }
+          //   }
+          // }
+          'postcss-loader'
+
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     postcssOptions: {
+          //       plugins: [
+          //         require('autoprefixer'),
+          //         require('postcss-preset-env')
+          //       ]
+          //     }
+          //   }
+          // },
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      // {
+      //   test: /\.(png|svg|gif|jpe?g)$/,
+      //   type: 'asset/resource',
+      //   generator: {
+      //     filename: 'img/[name].[hash:4][ext]'
+      //   }
+      // },
+      // {
+      //   test: /\.(png|svg|gif|jpe?g)$/,
+      //   type: 'asset/inline'
+      // }
+      { // 这一块的代码 30kb一下打包成base64,30kb以上打包copy成图片并在特定文件内
+        test: /\.(png|svg|gif|jpe?g)$/,
+        type: 'asset',
+        generator: {
+          filename: 'img/[name].[hash:4][ext]'
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 30 * 1024
+          }
+        }
+      },
+      {
+        test: /\.(ttf|woff2?|eot|otf)(\?.*)?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'font/[name].[hash:3][ext]'
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        type: 'asset/resource',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 30 * 1024
+          }
+        },
+        generator: {
+          filename: 'media/[name].[hash:3][ext]'
+        }
+      }
+    ]
+  }
+
 }
