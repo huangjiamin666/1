3632b7622291d69c11f762ae2898a7cfaf0c2213
h <178080976@qq.com>
2023-03-02 15:36:50 +0800

    xg

---
+++ 项目总结/el-drawer.vue
@@ -0,0 +1,12 @@
+<el-drawer
+:title="title"
+:modal-append-to-body="false"
+:append-to-body="true"
+:visible.sync="visualable"
+direction="rtl"
+class=""
+size="30%"
+:before-close="handleClose"
+>
+
+</el-drawer>
